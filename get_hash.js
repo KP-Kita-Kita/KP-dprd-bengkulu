eaf6439b18e2305ced68bb22496be3301617d0b4
Fx79 <147541049+Fx79@users.noreply.github.com>
2026-04-12 20:16:53 +0700

    Delete get_hash.js

--- get_hash.js
+++
@@ -1,8 +0,0 @@
-const bcrypt = require('bcryptjs');
-
-bcrypt.hash('indonesiaemas2045', 10).then(hash => {
-    console.log('\n=======================================');
-    console.log('HASIL HASH PASSWORD:');
-    console.log(hash);
-    console.log('=======================================\n');
-});
